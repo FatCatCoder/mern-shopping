ab9b3001e4d884b02099e7ca06091fcc95a2976f
FatCatCoder <christian75armand@gmail.com>
2021-05-20 13:53:55 -0400

    changes

--- routes/api/items.js
+++ routes/api/items.js
@@ -22,7 +22,7 @@ router.post('/', (req, res) => {
 
 //DELETE
 router.delete('/:id', (req, res) => {
-    Item.findById(req.paramas.id)
+    Item.findById(req.params.id)
     .then(item => item.remove().then(() => res.json({ success: true})))
     .catch(err => res.status(404).json({success: false}))
 })
